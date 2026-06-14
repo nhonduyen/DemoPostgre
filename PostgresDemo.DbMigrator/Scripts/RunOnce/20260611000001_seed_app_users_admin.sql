2163308d53faef2455bec5316deb8c0d9aae1f6e
nhonduyen <nhonduyen@gmail.com>
2026-06-14 19:40:58 +0700

    change admin id is 1

--- PostgresDemo.DbMigrator/Scripts/RunOnce/20260611000001_seed_app_users_admin.sql
+++ PostgresDemo.DbMigrator/Scripts/RunOnce/20260611000001_seed_app_users_admin.sql
@@ -1,6 +1,6 @@
 ﻿INSERT INTO app_user (id, name, username, password, salt)
 VALUES (
-    fn_next_snowflake_id(1),
+    1,
     'Administrator',
     'admin',
     '123456',  -- see below for generating this
